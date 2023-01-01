425365f0218ae1cb15e4b625aec5f724b512ca40
thereactsamurai <katanacoding@hotmail.com>
2023-01-01 18:55:15 +0500

    Changed project name

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 </script>
 
-<template></template>
+<template>
+  <RouterView />
+</template>
